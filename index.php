5032c77f7152cadb877548284f6f7cdb50c6128e
Shahid <shahidmughal0256@gmail.com>
2022-04-04 19:50:44 +0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Main template file
+*
+*@package Aquila
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <title>Main File</title>
+</head>
+<body>
+    <h1>Main File</h1>
+</body>
+</html>
