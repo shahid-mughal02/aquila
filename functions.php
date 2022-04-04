49c27107b7230a5019681b127eef9515dbf01fad
Shahid <shahidmughal0256@gmail.com>
2022-04-04 23:33:49 +0500

    Updated Files and added template parts

--- functions.php
+++ functions.php
@@ -12,12 +12,21 @@ function aquila_enqueue_scripts(){
     // wp_enqueue_style( 'style-css', get_stylesheet_uri(), null, filemtime(get_template_directory(). '/style.css'), 'all' );
     // wp_enqueue_script( 'main-js', get_template_directory_uri(). '/assets/main.js', null, filemtime(get_template_directory(). '/assets/main.js'), true );
     //OR
+    //Register Styles
     wp_register_style('style-css', get_stylesheet_uri(), null, filemtime(get_template_directory(). '/style.css'), 'all');
-    wp_register_script('main-js', get_template_directory_uri(). '/assets/main.js', null, filemtime(get_template_directory(). '/assets/main.js'), true );
+    wp_register_style('bootstrap-css', get_template_directory_uri(). '/assets/src/library/css/bootstrap.min.css', null, false , 'all');
     
+    //Register Scripts
+    wp_register_script('main-js', get_template_directory_uri(). '/assets/main.js', ['jquery'], filemtime(get_template_directory(). '/assets/main.js'), true );
+    wp_register_script('bootstrap-js', get_template_directory_uri(). '/assets/src/library/js/bootstrap.min.js', null, false, true );
+
+    //Enqueue Styles
     wp_enqueue_style('style-css');
+    wp_enqueue_style('bootstrap-css');
+
+    //Enqueue Sscript
     wp_enqueue_script('main-js');
-    
+    wp_enqueue_script('bootstrap-js');
 } 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts')
 ?>
--- header.php
+++ header.php
@@ -7,14 +7,24 @@
 ?>
 <!DOCTYPE html>
 <html lang="<?php language_attributes();?>">
+
 <head>
     <meta charset="<?php bloginfo('charset')?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head();?>
 </head>
-<body <?php body_class('hello-class'); ?>>
-    <?php if(function_exists('wp_body_open')):
-        wp_body_open();
-    endif; ?>
-    <header>Header</header>
+
+<body <?php body_class('hello-class');?>>
+    <?php if (function_exists('wp_body_open')):
+    wp_body_open();
+endif;?>
+
+    <div id="page" class="site">
+        <header id="mastheade" class="site-header" role="banner">
+            <?php get_template_part('template-parts/header/nav');?>
+        </header>
+        <div id="content" class="site-content">
+            <?php //get_template_part('template-parts/content', 'post'); ?>
+        </div>
+    </div>
