e4e15ff94f6aa5d3f85bfb59165b556c9d8cbd1a
Shahid <shahidmughal0256@gmail.com>
2022-04-05 15:33:20 +0500

    Updated Menus

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -25,24 +25,39 @@ class Menus
          * Actions
          *
          */
-        add_action( 'init', [$this, 'register_menus'] );
+        add_action('init', [$this, 'register_menus']);
     }
 
-    public function register_menus(){
+    public function register_menus()
+    {
         register_nav_menus([
-            'aquila-header-menu' => esc_html__('Header Menu', 'aquila'),    //esc_html__ = for translation
-            'aquila-footer-menu' => esc_html__('Footer Menu', 'aquila'),    //esc_html__ = for translation
+            'aquila-header-menu' => esc_html__('Header Menu', 'aquila'), //esc_html__ = for translation
+            'aquila-footer-menu' => esc_html__('Footer Menu', 'aquila'), //esc_html__ = for translation
         ]);
     }
 
-    public function get_menu_id( $location ){
+    public function get_menu_id($location)
+    {
         //Get all the locations
         $locations = get_nav_menu_locations();
-        
+
         //Get Object ID by location
         $menu_id = $locations[$location];
-        
-        return !empty( $menu_id ) ? $menu_id : '';
+
+        return !empty($menu_id) ? $menu_id : '';
+    }
+
+    public function get_child_menu_items($menu_array, $parent_id)
+    {
+        $child_menus = [];
+
+        if( !empty( $menu_array ) && is_array( $menu_array ) ){
+            foreach ($menu_array as $menu) {
+                if( intval($menu->menu_item_parent) === $parent_id ){
+                    array_push( $child_menus, $menu );
+                }
+            }
+        }
     }
 
 }
